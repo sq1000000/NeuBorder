6a18726774c6cbab50da7ff172fa85d1b2822332
Xav! <39637438+sq1000000@users.noreply.github.com>
2023-05-27 04:54:06 -0400

    Add files via upload

---
+++ compile.sh
@@ -0,0 +1,5 @@
+lessc main.less out.css
+mkdir /home/sq/Obsidian/Notes/.obsidian/themes/
+mkdir /home/sq/Obsidian/Notes/.obsidian/themes/NeuBorder/
+mv out.css /home/sq/Obsidian/Notes/.obsidian/themes/NeuBorder/theme.css
+cp src/manifest.json /home/sq/Obsidian/Notes/.obsidian/themes/NeuBorder/
